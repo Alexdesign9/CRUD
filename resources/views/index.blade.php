4513aa48ae7618d989a2e3a6a50a97e3776f1e2d
Alexey <39150549+Alexdesign9@users.noreply.github.com>
2019-07-24 02:07:37 +0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
   crossorigin="anonymous"></script>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
-  <script src="js/javascript.js" defer></script>
+  <script src="js/main.js" defer></script>
 </head>
 <body>
   <div class="overlay">
